36c560e08151ff0b7a01d15b166e2c62ac360841
Mike Nachbaur <mike.nachbaur@okta.com>
2021-02-12 11:15:44 -0800

    Added TokenRequest initializer that can be used with direct interaction code grant values

--- Tests/IDXClientRequestTests.swift
+++ Tests/IDXClientRequestTests.swift
@@ -47,4 +47,32 @@ class IDXClientRequestTests: XCTestCase {
         let state = data?["state"]
         XCTAssertNotNil(UUID(uuidString: state!!))
     }
+    
+    func testTokenRequestWithInteractionCode() throws {
+        let issuerUrl = try XCTUnwrap(URL(string: "https://example.com/oauth2/default"))
+        let request = IDXClient.APIVersion1.TokenRequest(issuer: issuerUrl,
+                                                         clientId: "MyClientId",
+                                                         clientSecret: "SsshItsSecret",
+                                                         codeVerifier: "ABCeasyas123",
+                                                         grantType: "interaction_code",
+                                                         code: "TheInteractionCode")
+
+        let urlRequest = try XCTUnwrap(request.urlRequest(using: configuration))
+        XCTAssertEqual(urlRequest.httpMethod, "POST")
+
+        let url = try XCTUnwrap(urlRequest.url?.absoluteString)
+        XCTAssertEqual(url, "https://example.com/oauth2/v1/token")
+        
+        XCTAssertEqual(urlRequest.allHTTPHeaderFields?["Content-Type"], "application/x-www-form-urlencoded")
+        XCTAssertEqual(urlRequest.allHTTPHeaderFields?["Accept"], "application/json")
+        XCTAssertNotNil(urlRequest.allHTTPHeaderFields?["User-Agent"])
+
+        let data = try XCTUnwrap(urlRequest.httpBody?.urlFormEncoded())
+        XCTAssertEqual(data.keys.sorted(), ["client_id", "client_secret", "code_verifier", "grant_type", "interaction_code"])
+        XCTAssertEqual(data["client_id"], "MyClientId")
+        XCTAssertEqual(data["client_secret"], "SsshItsSecret")
+        XCTAssertEqual(data["code_verifier"], "ABCeasyas123")
+        XCTAssertEqual(data["grant_type"], "interaction_code")
+        XCTAssertEqual(data["interaction_code"], "TheInteractionCode")
+    }
 }
