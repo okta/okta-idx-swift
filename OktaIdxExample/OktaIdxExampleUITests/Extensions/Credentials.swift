b387fc2aa70021bda463287d58b045b7a8168a66
Mike Nachbaur <mike.nachbaur@okta.com>
2021-05-18 16:07:27 -0700

    Add E2E code verification utility features

--- OktaIdxExample/OktaIdxExampleUITests/Extensions/Credentials.swift
+++ OktaIdxExample/OktaIdxExampleUITests/Extensions/Credentials.swift
@@ -36,6 +36,8 @@ struct TestCredentials {
     let clientId: String
     let issuer: String
     let redirectUri: String
+    let a18nAPIKey: String
+    let a18nProfileId: String
     var issuerUrl: String {
         return "https://\(issuer)/oauth2/default"
     }
@@ -45,6 +47,8 @@ struct TestCredentials {
         guard let clientId = env["CLIENT_ID"],
               let issuer = env["ISSUER_DOMAIN"],
               let redirectUri = env["REDIRECT_URI"],
+              let a18nAPIKey = env["A18N_API_KEY"],
+              let a18nProfileId = env["A18N_PROFILE_ID"],
               let username = env[scenario.usernameKey],
               let password = env[scenario.passwordKey] else
         {
@@ -54,6 +58,8 @@ struct TestCredentials {
         guard clientId != "",
               issuer != "",
               redirectUri != "",
+              a18nAPIKey != "",
+              a18nProfileId != "",
               username != "",
               password != "" else
         {
@@ -63,6 +69,8 @@ struct TestCredentials {
         self.clientId = clientId
         self.issuer = issuer
         self.redirectUri = redirectUri
+        self.a18nAPIKey = a18nAPIKey
+        self.a18nProfileId = a18nProfileId
         self.username = username
         self.password = password
     }
