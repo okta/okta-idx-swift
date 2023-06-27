bdac2a56460454eb1b69c45eab5d37fb5666f691
Mike Nachbaur <mike.nachbaur@okta.com>
2023-06-26 18:42:47 -0700

    Mark InteractionCodeFlow as an ObservableObject

--- Sources/OktaIdx/InteractionCodeFlow.swift
+++ Sources/OktaIdx/InteractionCodeFlow.swift
@@ -18,7 +18,7 @@ import Foundation
 /// This permits a user to be authenticated using a dynamic and customizable workflow that is driven by server-side policy configuration. A user is given choices in how they authenticate, how they verify one or more authentication factors, and can enable self-service registration and authenticator enrollment.
 ///
 /// This class is used to communicate which application, defined within Okta, the user is being authenticated with. From this point a workflow is initiated, consisting of a series of authentication ``Remediation`` steps. At each step, your application can introspect the ``Response`` object to determine which UI should be presented to your user to guide them through to login.
-public final class InteractionCodeFlow: AuthenticationFlow {
+public final class InteractionCodeFlow: ObservableObject, AuthenticationFlow {
     /// Options to use when initiating a ``InteractionCodeFlow`` sign in flow.
     ///
     /// These options enable you to customize certain attributes used during the beginning of a session.
