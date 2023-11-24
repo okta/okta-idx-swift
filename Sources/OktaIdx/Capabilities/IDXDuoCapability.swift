45b68db5f8c141e0e768c2f85c2231539b526b43
Mike Nachbaur <mike.nachbaur@okta.com>
2023-11-23 16:07:53 -0800

    Expand on Duo capability to enable simplified linking between the remediation and its authenticator capability.
    This gives the remediations an opportunity to fix up the state or perform validation before a request will be created.

--- Sources/OktaIdx/Capabilities/IDXDuoCapability.swift
+++ Sources/OktaIdx/Capabilities/IDXDuoCapability.swift
@@ -9,11 +9,23 @@ import Foundation
 
 extension Capability {
     /// Capability to access data related to Duo
-    public struct Duo: AuthenticatorCapability,RemediationCapability {
+    public struct Duo: AuthenticatorCapability {
         public let host: String
         public let signedToken: String
         public let script: String
+        public var signatureData: String?
         
-        let answerField: Remediation.Form.Field?
+        public func willProceed(to remediation: Remediation) {
+            guard remediation.authenticators.contains(where: {
+                $0.type == .app && $0.methods?.contains(.duo) ?? false
+            }),
+                  let credentialsField = remediation.form["credentials"],
+                  let signatureField = credentialsField.form?.allFields.first(where: { $0.name == "signatureData" })
+            else {
+                return
+            }
+            
+            signatureField.value = signatureData
+        }
     }
 }
